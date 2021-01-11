6df0725b25d39d855bd063225b109f590d7e782f
Vijay <vijay>
2021-01-11 18:58:12 +0530

    added modal

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+function signInModal() {
+    let modal = document.getElementById("myModal");
+    let btn = document.getElementById("sign_in");
+    let span = document.getElementsByClassName("close")[0];
+
+// sign-in button
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
