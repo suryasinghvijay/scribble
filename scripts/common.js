6e238e48c7ef579c2ea66972ec913de963015cb3
Vijay <vijay>
2021-01-12 16:43:51 +0530

    few changes with modal

--- scripts/common.js
+++ scripts/common.js
@@ -1,36 +1,44 @@
-let modal = document.getElementsByClassName("modal")[0];
-let btn = document.getElementById("sign_up");
-let span = document.getElementsByClassName("close")[0];
+let signUpModal = document.getElementsByClassName("modal")[0];
+let signUpSpan = document.getElementsByClassName("close")[0];
 let signInModal = document.getElementsByClassName("modal")[1];
-let signInBtn = document.getElementById("sign_in");
 let signInSpan = document.getElementsByClassName("close")[1];
 let register = document.getElementById("register");
+let createPostModal = document.getElementsByClassName("postCreateModal")[0];
+let createPostSpan = document.getElementsByClassName("close")[2];
 
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    } else if (event.target === signInModal) {
-        signInModal.style.display = "none";
+
+function displayModal(model, span) {
+    console.log("create post model"+ model)
+
+    model.style.display = "block";
+
+    span.onclick = function () {
+        model.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target === model) {
+            model.style.display = "none";
+        }
     }
 }
 
-signInBtn.onclick = function () {
-    signInModal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-signInSpan.onclick = function () {
-    signInModal.style.display = "none";
-}
-
-register.onclick = function () {
-    signInModal.style.display = "none";
-    modal.style.display = "block";
+function showModal(id) {
+    console.log("create post id"+ id)
+    switch (id) {
+        case 'sign_in':
+            displayModal(signInModal, signInSpan)
+            register.onclick = function () {
+                signInModal.style.display = "none";
+                signUpModal.style.display = "block";
+            }
+            break;
+        case  'sign_up':
+            displayModal(signUpModal, signUpSpan)
+            break;
+        case 'create_post':
+            console.log("create post")
+            displayModal(createPostModal, createPostSpan)
+            break;
+    }
 }
